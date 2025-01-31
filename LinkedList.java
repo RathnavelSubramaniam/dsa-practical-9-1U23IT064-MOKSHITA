1c9546253f5844bbc5a5c093ec195f3d3e1900d9
2023ITAMOKSHITA <139752155+1U23IT064-MOKSHITA@users.noreply.github.com>
2025-01-31 06:39:48 +0000

    DONE9

--- LinkedList.java
+++ LinkedList.java
@@ -46,14 +46,20 @@ sc.close();
     }
 public void deleteBegin()
 {
-if(head==null)
-System.out.println("List is empty");
-else
-{
-    Node temp=head;
-    head=head.next;
-    System.out.println("Deleted Element is;"+temp.data);
-}
+    if (head == null)
+    {
+        System.out.println("List is Empty");
+    }
+    else
+    {
+       Node temp = head;
+       head = head.next;
+       if (head == null)
+       {
+        tail = null;
+       }
+       System.out.println("Deleted Element is:" + temp.data);
+     }
 }
 public void displayList() {
         Node current = head;
